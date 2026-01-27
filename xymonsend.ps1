1a52a2361e7531f67052e6a0698dc7434065f28c
Stef Coene <scne@axi.be>
2026-01-27 21:23:01 +0100

    Import WinPSClient from xymon svn mirror sandbox

---
+++ xymonsend.ps1
@@ -0,0 +1,59 @@
+﻿function XymonSend($msg, $servers)
+{
+	$saveresponse = 1	# Only on the first server
+	$outputbuffer = ""
+	$ASCIIEncoder = New-Object System.Text.ASCIIEncoding
+
+	foreach ($srv in $servers) {
+		$srvparams = $srv.Split(":")
+		# allow for server names that may resolve to multiple A records
+		$srvIPs = & {
+			$local:ErrorActionPreference = "SilentlyContinue"
+			$srvparams[0] | %{[system.net.dns]::GetHostAddresses($_)} | %{ $_.IPAddressToString}
+		}
+		if ($srvIPs -eq $null) { # no IP addresses could be looked up
+			Write-Error -Category InvalidData ("No IP addresses could be found for host: " + $srvparams[0])
+		} else {
+			if ($srvparams.Count -gt 1) {
+				$srvport = $srvparams[1]
+			} else {
+				$srvport = 1984
+			}
+			foreach ($srvip in $srvIPs) {
+
+				$saveerractpref = $ErrorActionPreference
+				$ErrorActionPreference = "SilentlyContinue"
+				$socket = new-object System.Net.Sockets.TcpClient
+				$socket.Connect($srvip, $srvport)
+				$ErrorActionPreference = $saveerractpref
+				if(! $? -or ! $socket.Connected ) {
+					$errmsg = $Error[0].Exception
+					Write-Error -Category OpenError "Cannot connect to host $srv ($srvip) : $errmsg"
+					continue;
+				}
+				$socket.sendTimeout = 500
+				$socket.NoDelay = $true
+
+				$stream = $socket.GetStream()
+				
+				$sent = 0
+				foreach ($line in $msg) {
+					# Convert data to ASCII instead of UTF, and to Unix line breaks
+					$sent += $socket.Client.Send($ASCIIEncoder.GetBytes($line.Replace("`r","") + "`n"))
+				}
+
+				if ($saveresponse-- -gt 0) {
+					$socket.Client.Shutdown(1)	# Signal to Xymon we're done writing.
+
+					$s = new-object system.io.StreamReader($stream,"ASCII")
+
+					start-sleep -m 200  # wait for data to buffer
+					$outputBuffer = $s.ReadToEnd()
+				}
+
+				$socket.Close()
+			}
+		}
+	}
+	$outputbuffer
+}
